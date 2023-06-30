aa41753a1668ff625a08f718fc802af09698cead
Yasser-Hamody <yasserhamody3@gmail.com>
2023-07-01 01:02:39 +0300

    Adding the route for the client login

---
+++ Routes/ClientLogin.js
@@ -0,0 +1,7 @@
+// const express = require('express');
+// const router = express.Router();
+// const { Client_login } = require('../Controllers/Login');
+
+// console.log('hello');
+// router.post('/Login', Client_login);
+// module.exports = router;
