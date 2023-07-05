2024c30775807106139890981a0495e3cb81948b
Yasser-Hamody <yasserhamody3@gmail.com>
2023-07-05 18:52:19 +0300

    Remove the ClientLogin route

--- Routes/ClientLogin.js
+++
@@ -1,7 +0,0 @@
-// const express = require('express');
-// const router = express.Router();
-// const { Client_login } = require('../Controllers/Login');
-
-// console.log('hello');
-// router.post('/Login', Client_login);
-// module.exports = router;
